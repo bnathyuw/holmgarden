6046e177f398c906a0180d9f54ef8548446c6016
Matthew Butt <matthew@matthewbutt.com>
2016-11-10 17:23:16 +0000

    Refactor JS

--- js/HolmGarden.js
+++ js/HolmGarden.js
@@ -1,4 +1,8 @@
-﻿$(function() {
-	$(".date.booking").datepicker({ dateFormat: "dd/mm/yy", minDate: "+1d", maxDate: "+3y" });
-	$(".date.dob").datepicker({ dateFormat: "dd/mm/yy", minDate: "-120y", maxDate: "-18y", changeYear: true, yearRange: "-120:-18" });
+﻿var setUpDatePickers = function(){
+    $(".date.booking").datepicker({ dateFormat: "dd/mm/yy", minDate: "+1d", maxDate: "+3y" });
+    $(".date.dob").datepicker({ dateFormat: "dd/mm/yy", minDate: "-120y", maxDate: "-18y", changeYear: true, yearRange: "-120:-18" });
+};
+
+$(function() {
+    setUpDatePickers();
 });
