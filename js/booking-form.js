f2bfb6856ec33a683f90fec861ddf8f53f023a9c
Matthew Butt <matthew@matthewbutt.com>
2016-11-27 18:36:21 +0000

    Add jQuery form validation to Booking form

--- js/booking-form.js
+++ js/booking-form.js
@@ -1,4 +1,7 @@
-﻿var setUpDatePickers = function(){
+﻿$.validate({
+    borderColorOnError: ''
+});
+var setUpDatePickers = function(){
     $(".date.booking").datepicker({ dateFormat: "dd/mm/yy", minDate: "+1d", maxDate: "+3y" });
     $(".date.dob").datepicker({ dateFormat: "dd/mm/yy", minDate: "-120y", maxDate: "-18y", changeYear: true, yearRange: "-120:-18" });
 };
